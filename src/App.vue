5f9b29efd439e0606657f1c7a58b485cb5c36766
coder004 <30897961+coder004@users.noreply.github.com>
2019-08-28 03:49:29 +0530

    creating scss style files, added assets

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import "style/main.scss";
+
+html {
+  scroll-behavior: smooth;
+}
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  color: #ffeaea;
 }
 </style>
+
+<script lang="ts">
+import Vue from "vue";
+var json = require("@/static/content.json");
+// @ is an alias to /src
+
+export default Vue.extend({
+  name: "home",
+  components: {},
+  data() {
+    return {
+      json: {}
+    };
+  },
+  mounted() {
+    this.json = json;
+  }
+});
+</script>
