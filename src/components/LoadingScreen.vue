6d1976dcfbb0992a2daaec4d07438ff20beef4ec
coder004 <30897961+coder004@users.noreply.github.com>
2020-05-08 21:25:36 -0400

    fixed small text mistakes

---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="{ loader: true, fadeout: !isLoading }">Loading ...</div>
+</template>
+
+<script>
+export default {
+  name: "LoadingScreen",
+  props: ["isLoading"]
+};
+</script>
+
+<style lang="scss">
+@import "../styles/main.scss";
+.loader {
+  background-color: $darkBlueColor;
+  bottom: 0;
+  color: white;
+  display: block;
+  font-size: 32px;
+  left: 0;
+  overflow: hidden;
+  padding-top: 10vh;
+  position: fixed;
+  right: 0;
+  text-align: center;
+  top: 0;
+}
+
+.fadeout {
+  animation: fadeout 2s forwards;
+}
+
+@keyframes fadeout {
+  to {
+    opacity: 0;
+    visibility: hidden;
+  }
+}
+</style>
