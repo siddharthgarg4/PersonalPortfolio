ccf30e54d5f96ede22e06d03f81c35ba30856847
coder004 <30897961+coder004@users.noreply.github.com>
2024-12-26 20:53:50 -0500

    removed unnecessary files and from components.d.ts

--- components.d.ts
+++ components.d.ts
@@ -17,16 +17,8 @@ declare module 'vue' {
     CustomCard: typeof import('./src/components/CustomCard.vue')['default']
     CustomMenu: typeof import('./src/components/CustomMenu.vue')['default']
     ExperienceAndProjectsSection: typeof import('./src/components/ExperienceAndProjectsSection.vue')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    IconCommunity: typeof import('./src/components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./src/components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./src/components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./src/components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./src/components/icons/IconTooling.vue')['default']
     LoadingScreen: typeof import('./src/components/LoadingScreen.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    TheWelcome: typeof import('./src/components/TheWelcome.vue')['default']
-    WelcomeItem: typeof import('./src/components/WelcomeItem.vue')['default']
   }
 }
