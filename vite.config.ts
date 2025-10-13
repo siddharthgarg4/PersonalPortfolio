c9ba73cf9e677ffd341078333ebae7a99ed3e51e
coder004 <30897961+coder004@users.noreply.github.com>
2025-10-13 02:19:05 -0400

    added vite package to optimize images on npm run build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import Components from 'unplugin-vue-components/vite'
 import {BootstrapVueNextResolver} from 'bootstrap-vue-next'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +14,21 @@ export default defineConfig({
     Components({
       resolvers: [BootstrapVueNextResolver()],
       dts: true,
-    })],
+    }),
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpg: { quality: 75 },
+      jpeg: { quality: 75 },
+      webp: { quality: 80 },
+      avif: { quality: 70 },
+      svg: {                 
+        plugins: [
+          { name: 'removeViewBox' },
+          { name: 'sortAttrs' },
+        ],
+      },
+    })
+  ],
   server: {
     port: 2025,
     host: '0.0.0.0',
