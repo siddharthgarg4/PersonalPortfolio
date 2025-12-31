dbd8ec30c1a34bfb466d74f449ed2b10f5ef2000
Siddharth Garg <siddharthgarg@Siddharths-MacBook-Pro.local>
2025-12-30 21:13:23 -0500

    added backup bg-color to app, descoped loading screen scss and moved to main.scss; also CONFIRMED the html flicker during load before loader is dev-only issue since priority on speed in vue-dev build

--- src/components/LoadingScreen.vue
+++ src/components/LoadingScreen.vue
@@ -29,32 +29,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style scoped lang="scss">
-@use "@/assets/styles/variables.scss" as *;
-.loader {
-  //background
-  inset: 0; //sets top, right, bottom, left to 0
-  position: fixed;
-  height: 100%;
-  width: 100%;
-  background-color: $dolphinBlueColor;
-  //loading image
-  background-image: url("@/assets/loading-moving-car.svg");
-  background-position: center;
-  background-repeat: no-repeat;
-  //safety settings
-  overflow: hidden;
-  z-index: 9999; // should be at the top of every element
-  pointer-events: auto;
-}
-.fadeoutLoader {
-  opacity: 0;
-  visibility: hidden;
-  transition:
-    opacity 2s ease,
-    visibility 2s ease;
-  //allow click through
-  pointer-events: none;
-}
-</style>
